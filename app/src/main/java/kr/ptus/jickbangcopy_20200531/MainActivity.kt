38f5201eb93ccd5e0322a807019814a37196fb7d
jaehwan0103 <dkdlfkqb1@gmail.com>
2020-05-31 12:49:29 +0900

    리스트뷰 / 어댑터 실제 연결

--- app/src/main/java/kr/ptus/jickbangcopy_20200531/MainActivity.kt
+++ app/src/main/java/kr/ptus/jickbangcopy_20200531/MainActivity.kt
@@ -2,40 +2,43 @@ package kr.ptus.jickbangcopy_20200531
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.ptus.jickbangcopy_20200531.adapters.RoomAdapter
 import kr.ptus.jickbangcopy_20200531.data.roomdata
 
 class MainActivity : BaseActivity() {
 
     val rooms = ArrayList<roomdata>()
-
+    lateinit var RoomAdapter: RoomAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupEvent()
         setValues()
+
     }
 
     override fun setupEvent() {
-
 
 
     }
 
     override fun setValues() {
 
-        rooms.add(roomdata(8000,"마포구 서교동",1,"마포 서교동의 방입니다."))
-        rooms.add(roomdata(23000,"마포구 서교동",3,"마포 서교동의 방입니다."))
-        rooms.add(roomdata(14000,"마포구 서교동",5,"마포 서교동의 방입니다."))
-        rooms.add(roomdata(16000,"마포구 서교동",4,"마포 서교동의 방입니다."))
-        rooms.add(roomdata(6000,"마포구 성산동",0,"마포 성산동의 방입니다."))
-        rooms.add(roomdata(45000,"마포구 성산동",1,"마포 성산동의 방입니다."))
-        rooms.add(roomdata(23000,"마포구 성산동",0,"마포 성산동의 방입니다."))
-        rooms.add(roomdata(12000,"마포구 망원동",-1,"마포 망원동의 방입니다."))
-        rooms.add(roomdata(8000,"마포구 망원동",-1,"마포 망원동의 방입니다."))
-        rooms.add(roomdata(6000,"마포구 망원동",2,"마포 망원동의 방입니다."))
+        rooms.add(roomdata(8000, "마포구 서교동", 1, "마포 서교동의 방입니다."))
+        rooms.add(roomdata(23000, "마포구 서교동", 3, "마포 서교동의 방입니다."))
+        rooms.add(roomdata(14000, "마포구 서교동", 5, "마포 서교동의 방입니다."))
+        rooms.add(roomdata(16000, "마포구 서교동", 4, "마포 서교동의 방입니다."))
+        rooms.add(roomdata(6000, "마포구 성산동", 0, "마포 성산동의 방입니다."))
+        rooms.add(roomdata(45000, "마포구 성산동", 1, "마포 성산동의 방입니다."))
+        rooms.add(roomdata(23000, "마포구 성산동", 0, "마포 성산동의 방입니다."))
+        rooms.add(roomdata(12000, "마포구 망원동", -1, "마포 망원동의 방입니다."))
+        rooms.add(roomdata(8000, "마포구 망원동", -1, "마포 망원동의 방입니다."))
+        rooms.add(roomdata(6000, "마포구 망원동", 2, "마포 망원동의 방입니다."))
 
+        RoomAdapter = RoomAdapter(mContext,R.layout.roomlist,rooms)
 
-
+        roomListView.adapter = RoomAdapter
 
     }
 
