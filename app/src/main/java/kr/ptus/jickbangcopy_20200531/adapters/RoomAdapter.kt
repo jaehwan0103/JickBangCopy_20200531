789a5714bb320338463c2ae054e49a8d07c4bc9c
jaehwan0103 <dkdlfkqb1@gmail.com>
2020-05-31 12:01:59 +0900

    어댑터 생성자 / 상속 처리

---
+++ app/src/main/java/kr/ptus/jickbangcopy_20200531/adapters/RoomAdapter.kt
@@ -0,0 +1,11 @@
+package kr.ptus.jickbangcopy_20200531.adapters
+
+import android.content.Context
+import android.content.LocusId
+import android.widget.ArrayAdapter
+import kr.ptus.jickbangcopy_20200531.data.roomdata
+
+class RoomAdapter (context: Context, resId: Int, list : List<roomdata>) : ArrayAdapter<roomdata>(context,resId,list) {
+
+
+}
